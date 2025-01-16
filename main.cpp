c60c6cbb7bcd0ed286e82a664c67291f8033fadf
Sakura_Dusk <sakura_dusk@sjtu.edu.cn>
2025-01-16 18:27:02 +0800

    Update code about MemoryRiver

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include<fstream>
+#include<iostream>
+#include<cstring>
+#include<string>
+#include "filestore.h"
+
+// int main() {
+//     std::ofstream inp;
+//     inp.open("read.txt", std::ios::out);
+//     if (!inp.is_open()) {
+//         std::ofstream inp;
+//         inp.open("read.txt", std::ios::out);
+//     }
+//     int n;
+//     std::cin >> n;
+//     inp << n;
+//     std::string line;
+//     while (getline(std::cin, line)) {
+//         inp << line << std::endl;
+//     }
+//     inp.close();
+// }
+
+int main() {
+    int n; std::cin >> n;
+    for (int i = 1; i <= n; i++) {
+        std::string op; std::cin >> op;
+        if (op == "insert") {
+            std::string index; int value; std::cin >> index >> value;
+            data_inert(index, value);
+            continue;
+        }
+        if (op == "delete") {
+            std::string index; int value; std::cin >> index >> value;
+            data_delete(index, value);
+            continue;
+        }
+        if (op == "find") {
+            std::string index; std::cin >> index;
+            data_find(index);
+        }
+    }
+}
